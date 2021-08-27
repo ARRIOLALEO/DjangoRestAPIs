4dfcb3f9be5c021e1078ecb7f28b72207407027d
arriolaleo <riblarsv@gmail.com>
2021-08-27 12:09:52 +0600

    rest framework is configured

--- todoapp/backend/config/config/settings.py
+++ todoapp/backend/config/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gzdb*$(&sbe1afa=uo#bd)ez46kr&43^25la+by=1kr=kkznf='
+SECRET_KEY = 'django-insecure-tvc*zf30s9%71792sjb$dlfnm4dn2*qyu_onr(jp#%(qwe$*ht'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,10 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # local aps 
+
+    # 3party apps
+    'rest_framework',
+    # local apps
     'todos',
 ]
-
+REST_FRAMEWORK={
+    'DEFAULT_PERMISSIONS_CLASSES':[
+        'rest_framework.permisions.AllowAny',
+    ]
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
