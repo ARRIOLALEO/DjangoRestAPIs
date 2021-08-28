b8d4afa8a85f4c0b12740af8c5b7fa7296335472
arriolaleo <riblarsv@gmail.com>
2021-08-28 19:08:12 +0600

    basic structure of an api

--- basicapi/config/books/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import fields, serializers
-from .models import Book
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ('title','author','description')
