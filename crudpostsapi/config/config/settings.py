e610033f50ef7784774023d751d5351262b476ff
arriolaleo <riblarsv@gmail.com>
2021-09-01 14:37:15 +0600

    secret removed

--- crudpostsapi/config/config/settings.py
+++ crudpostsapi/config/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o9$tco*!#5xn&fbp#a87o3ez5_7^%k$4-!f4ia)=2jo@@m=t8k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
